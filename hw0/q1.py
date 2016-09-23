653ac9594109fd0ec322fd5796fe7d845df2cd48
alex84726 <b02901053@ntu.edu.tw>
2016-09-23 15:14:14 +0800

    revise-the-saving-way

--- hw0/q1.py
+++ hw0/q1.py
@@ -5,4 +5,4 @@ column = int(sys.argv[1])
 inputfile = sys.argv[2]
 dataset = numpy.loadtxt(inputfile,usecols=(column,))
 ans = numpy.sort(dataset)
-numpy.savetxt("ans1.txt",ans.flatten(),newline=',')
+ans.tofile("ans1.txt",sep=",")
