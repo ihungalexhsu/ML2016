b9ac11b6b64e345796b74ad6c9d2b423896b45c8
alex84726 <b02901053@ntu.edu.tw>
2016-10-28 17:31:30 +0800

    abc

--- hw2/train.py
+++ hw2/train.py
@@ -26,7 +26,6 @@ def sigmoid(x):
     return 1.0/(1.0+np.exp(-x))
 
 def estimate(wei, bi, feature):
-    #weight as matrix; bias is value ; feature as matrix
     temp = np.dot(wei, feature)
     temp = temp + bi
     temp = sigmoid(temp)
