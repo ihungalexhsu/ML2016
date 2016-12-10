ab1c297533aff38a7e5b42a330c6cee06f6950a8
eric-yyjau <eric.yyjau@gmail.com>
2016-12-10 13:11:30 +0800

    Eric_1210_work on validation

---
+++ final/src/Transfer_Learning_on_Stack_Exchange_Tags/test.sh
@@ -0,0 +1,8 @@
+python3 test.py $1 $2 $3
+
+command <<HERE
+# $1: directory path contains (all_label.p, all_unlabel.p, test.p) 
+# $2: input_model
+# $3: prediction.csv
+# ./test.sh data/ model prediction.csv
+HERE
