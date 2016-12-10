89ad3279c7d6556203a58890b28c213a6dabcd26
eric-yyjau <eric.yyjau@gmail.com>
2016-12-10 13:11:30 +0800

    Eric_1210_work on validation

---
+++ final/src/Transfer_Learning_on_Stack_Exchange_Tags/f1_score.py
@@ -0,0 +1,14 @@
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import f1_score
+from numpy import genfromtxt
+
+import sys
+
+
+
+ans_truth = genfromtxt( sys.argv[1], delimiter=',') 
+ans_truth = ans_test[1:, 1:]
+
+ans_test = genfromtxt( sys.argv[2], delimiter=',') 
+ans_test = ans_test[1:, 1:]
