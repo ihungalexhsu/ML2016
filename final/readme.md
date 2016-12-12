157df5ddb4cfba56fdf1d0388c72ade91c3415ff
alex <b02901053@ntu.edu.tw>
2016-12-12 11:52:28 +0800

    fucking pull

--- final/readme.md
+++
@@ -1,2 +0,0 @@
-#Final Project for NTU Mechine Learning Course
-
--- final/src/Transfer_Learning_on_Stack_Exchange_Tags/test.sh
+++
@@ -1,8 +0,0 @@
-python3 test.py $1 $2 $3
-
-command <<HERE
-# $1: directory path contains (all_label.p, all_unlabel.p, test.p) 
-# $2: input_model
-# $3: prediction.csv
-# ./test.sh data/ model prediction.csv
-HERE
